a7d86ff1ff3d9d37d01159e2a9261187de5e957f
Alexis Liu <aliu41@student.ubc.ca>
2024-08-22 18:58:59 -0400

    Finished getting book details from open library and getting multiple book results from an input from both libraries

--- search_google_books.py
+++ search_google_books.py
@@ -2,7 +2,7 @@ import requests
 import json
 from flask import Flask, request, jsonify
 
-GOOGLE_API_KEY = ''
+GOOGLE_API_KEY = '123'
 GOOGLE_URL = f'https://www.googleapis.com/books/v1/volumes'
 
 OPEN_LIBRARY_URL = 'https://openlibrary.org'
@@ -10,7 +10,7 @@ OPEN_LIBRARY_URL = 'https://openlibrary.org'
 app = Flask(__name__)
 
 
-def search_google_books(book_id):
+def get_google_book(book_id):
     response = requests.get(
         f'{GOOGLE_URL}/{book_id}', params={'key': GOOGLE_API_KEY})
     if response.status_code == 200:
@@ -20,52 +20,71 @@ def search_google_books(book_id):
 
 
 def get_open_library_book(book_id):
-    response = requests.get(f'{OPEN_LIBRARY_URL}/books/{book_id}.json')
+    response = requests.get(f'{OPEN_LIBRARY_URL}/works/{book_id}.json')
     if response.status_code == 200:
         return response.json()
     else:
         return f'Error: {response.status_code}'
 
 
-# def search_open_library(query):
-#     encoded_query = query.replace(' ', '+')
-#     response = requests.get(OPEN_LIBRARY_URL, params={'q': encoded_query})
-#     if response.status_code == 200:
-#         return jsonify(response.json())
-#     else:
-#         return f'Error: {response.status_code}'
-
-
-@app.route('/book/<book_id>', methods=['GET'])
+@app.route('/google_book/<book_id>', methods=['GET'])
 def get_google_book_details(book_id):
-    book = search_google_books(book_id)
+    book = get_google_book(book_id)
     volume_info = book.get('volumeInfo')
     title = volume_info.get('title')
     author = volume_info.get('authors')
     average_rating = volume_info.get("averageRating", "No rating available")
     ratingsCount = volume_info.get('ratingsCount')
-    return {'title': title, 'Author(s)': author, 'rating': average_rating, 'Ratings Count': ratingsCount}
+    return {'title': title, 'Author(s)': author, 'rating': average_rating, 'Ratings Count': ratingsCount, 'Book Id' : book_id}
 
+def get_author_name(author_key):
+    author_data = requests.get(f'{OPEN_LIBRARY_URL}{author_key}.json')
+    return author_data.json()
 
-@app.route('/openlibrary', methods=['GET'])
+def get_rating_summary(work_id):
+    ratings_response = requests.get(f'{OPEN_LIBRARY_URL}{work_id}/ratings.json')
+    rating_data = ratings_response.json()
+    rating_summary = rating_data.get('summary')
+    return rating_summary
+
+@app.route('/openlibrary_book', methods=['GET'])
 def get_open_library_book_details():
     book_id = request.args.get('book_id')
     book = get_open_library_book(book_id)
-    return book
+
+    work_id = book.get('works')[0].get('key')
+
+    title = book.get('title')
+    author = get_author_name(book.get('authors')[0].get('key'))
+    author_name = author.get('name')
+
+    ratings_summary = get_rating_summary(work_id)
+    average_rating = ratings_summary.get('average', 'no rating available')
+    ratingsCount = ratings_summary.get('count', 'No ratings count available')
+
+    return {'title': title, 'Author(s)': author_name, 'Ratings' : average_rating, 'Ratings Count': ratingsCount, 'work_id' : work_id}
 
 
-# @app.route('/search', methods=['GET'])
-# def search_book():
-#     query = request.args.get('query') or request.args.get('q')
+@app.route('/search_google', methods=['GET'])
+def search_google_books():
+    query = request.args.get('q')
+    if query is None:
+        return 'Need query paramter'
+    google_response = requests.get(f'{GOOGLE_URL}', params={'q':query, 'key' : GOOGLE_API_KEY})
+    if google_response.status_code == 200:
+        return google_response.json()
+    else:
+        return f'Error: {google_response.status_code}'
+    
 
-#     if query == None:
-#         return jsonify({'error': 'query parameter is required'})
-
-#     google_data = search_google_books(query)
-#     open_library_data = search_open_library(query)
-
-#     return google_data
-
+@app.route('/search_openlibrary', methods=['GET'])
+def search_open_library():
+    query = request.args.get('q')
+    response = requests.get(f'{OPEN_LIBRARY_URL}/search.json?', params={'q': query})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f'Error: {response.status_code}'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tets.py
+++ tets.py
@@ -1,8 +1,9 @@
 import requests
+import json
 
 # Set your API key and URL
-url = 'https://www.googleapis.com/books/v1/volumes'
-key = 'AIzaSyBKGQBM9WjJRZpclWTOKPOZfa_qVj0wLcY'
+GOOGLE_URL = 'https://www.googleapis.com/books/v1/volumes?'
+GOOGLE_API_KEY = '123'
 book_id = 'zyTCAlFPjgYC?'
 
 # # Make the request
@@ -16,15 +17,27 @@ book_id = 'zyTCAlFPjgYC?'
 #     print(f"Error {response.status_code}: {response.text}")
 
 
-def search_google_books(query):
-  response = requests.get(
-    f'{url}/{query}', params={'key': key})
-  if response.status_code == 200:
-    print(response.json())
-  else:
-    return f'Error: {response.status_code}'  
+# def search_google_books(query):
+#   response = requests.get(
+#     f'{url}/{query}', params={'key': key})
+#   if response.status_code == 200:
+#     print(response.json())
+#   else:
+#     return f'Error: {response.status_code}'
 
 
-search_google_books('zyTCAlFPjgYC')
+# search_google_books('zyTCAlFPjgYC')
+
+def search_google_books():
+    query = 'flowers'
+    if query is None:
+        return 'Need query paramter'
+    google_response = requests.get(f'{GOOGLE_URL}', params={
+                                   'q': query, 'key': GOOGLE_API_KEY})
     
-    
+    return google_response.json()
+
+
+
+str = json.dumps(search_google_books(), indent=4)
+print(str)
